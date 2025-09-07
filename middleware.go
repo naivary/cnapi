316f52e2aca7caf7419dbd945203a1312c399d67
Sayed Mustafa Hussaini <SayedMustafaHussaini@outlook.de>
2025-09-07 23:22:57 +0200

    done

--- middleware.go
+++ middleware.go
@@ -2,36 +2,8 @@ package main
 
 import "net/http"
 
-func timeout(next http.Handler) http.Handler {
-	return nil
-}
+package main 
 
-func requestID(next http.Handler) http.Handler {
-	return nil
-}
-
-func log(next http.Handler) http.Handler {
-	return nil
-}
-
-func cors(next http.Handler) http.Handler {
-	return nil
-}
-
-func contentType(next http.Handler) http.Handler {
-	return nil
-}
-
-func authz(next http.Handler) http.Handler {
-	return nil
-}
-
-func authn(next http.Handler) http.Handler {
-	return nil
-}
-
-// IsCanceled checks if the request context returns an error. If an error is
-// returned the processed response by the handler before will not be sent.
-func IsCanceled(next http.Handler) http.Handler {
-	return nil
+func middleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {})
 }

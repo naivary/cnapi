5fbb11481c63ba050f7bcbe78912c12f148963ed
Sayed Mustafa Hussaini <SayedMustafaHussaini@outlook.de>
2025-10-08 16:58:15 +0200

    need to add the documetnation possiblity to the endpoint

--- openapi/openapi.go
+++ openapi/openapi.go
@@ -1,4 +1,4 @@
-package main
+package openapi
 
 type OpenAPISecurityType int
 
--- routes.go
+++ routes.go
@@ -5,5 +5,4 @@ import "net/http"
 func addRoutes(mux *http.ServeMux) {
 	mux.Handle("GET /livez", livez())
 	mux.Handle("GET /readyz", readyz())
-
 }
